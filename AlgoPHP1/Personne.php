2010df09bf75fa8c1fd7d19ea4038ca08edbdad8
TimLaine <timothee.laine@gmail.com>
2023-06-14 16:56:14 +0200

    exo15 WIP, created classes, dates still don't work

--- AlgoPHP1/Personne.php
+++ AlgoPHP1/Personne.php
@@ -1,3 +1,29 @@
 <?php
+Class Personne{
+    private $_nom;
+    private $_prenom;
+    private $_age;
 
+    public function __construct ($nom,$prenom,$age){
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_age = $age;
+    }
+    public function getNom(){
+        return $this->_nom;
+    }
+    public function getPrenom(){
+        return $this->_prenom;
+    }
+    public function getAge(){
+        return $this->_age;
+    }
+    public function setAge($age){
+        $this->_age = $age;
+    }
+}
+$p1 = new Personne("DUPONT","Michel","1980-02-19");
+$p2 = new Personne("DUCHEMIN","Alice","1985-01-17");
+$p1->setAge("1996-03-23");
+echo $p1->getPrenom()." ". $p1->getNom()." a ".$p1->getAge()." ans";
 ?>
--- AlgoPHP1/exo15.php
+++ AlgoPHP1/exo15.php
@@ -11,5 +11,38 @@ Alice DUCHEMIN a … ans<br></p>
 <h2>Résultat</h2>
 
 <?php
+Class Personne{
+    private $_nom;
+    private $_prenom;
+    private $_age;
 
+    public function __construct ($nom,$prenom,$age){
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_age = $age;
+    }
+    public function getNom(){
+        return $this->_nom;
+    }
+    public function getPrenom(){
+        return $this->_prenom;
+    }
+    public function getAge(){
+        return $this->_age;
+    }
+    public function setAge($age){
+        $this->_age = $age;
+    }
+}
+$date = new DateTime();
+$age = new DateTime('01-01-2000');
+$vieux = $date->diff($age);
+$p1 = new Personne("DUPONT","Michel","1980-02-19");
+$p2 = new Personne("DUCHEMIN","Alice","1985-01-17");
+$p1->setAge("1996-03-23");
+$age = strtotime($p1->getPrenom());
+echo date('d/M/Y', $age);
+// echo $p1->getPrenom()." ". $p1->getNom()." a ".$p1->getAge()." ans<br>";
+// echo $p2->getPrenom()." ". $p2->getNom()." a ".$p2->getAge()." ans";
+// echo "<br>".$vieux->y;
 ?>
