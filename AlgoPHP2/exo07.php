0ea2d22697fc1fff264a1e5595d92dc9589a1d2e
TimLaine <timothee.laine@gmail.com>
2023-06-15 14:28:35 +0200

    Noticed Algo2 exo shouldn't be in Algo1 folder

---
+++ AlgoPHP2/exo07.php
@@ -0,0 +1,9 @@
+<h1>Exercice 7</h1>
+<p>Créer une fonction personnalisée permettant de générer des cases à cocher. On pourra préciser dans le tableau associatif si la case est cochée ou non.
+</p>
+<p>Exemple :
+genererCheckbox($elements); <br>
+//où $elements est un tableau associatif clé => valeur avec 3 entrées.
+</p>
+<h2>Résultat</h2>
+
